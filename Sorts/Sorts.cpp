03120e8737e16dbffd8aa3b77b6f154fcca539bb
MurzinDmitrii <114308567+MurzinDmitrii@users.noreply.github.com>
2024-12-29 17:58:33 +0300

    add insert and count sorts

--- Sorts/Sorts.cpp
+++ Sorts/Sorts.cpp
@@ -9,6 +9,7 @@ void print(int list[5], string nameSort)
     for (int i = 0; i < 5; i++) {
         cout << list[i] << " ";
     }
+    cout << "\n";
 }
 
 
@@ -38,8 +39,37 @@ void selectionSort()
     print(list, "selectionSort");
 }
 
+void insertionSort()
+{
+    int list[5] = { 5, 2, 4, 1, 3 };
+    for (int i = 1; i < 5; i++) {
+        for (int j = i; j > 0 && list[j - 1] > list[j]; j--) {
+            swap(list[j], list[j - 1]);
+        }
+    }
+    print(list, "insertionSort");
+}
+
+void countSort()
+{
+    int list[5] = { 3, 1, 2, 1, 3};
+    int countList[3] = { 0 }; //Потому что гарантировано что вариантов значений только 3
+    for (int i = 0; i < 5; i++) {
+        countList[list[i] - 1]++;
+    }
+    int j = 0;
+    for (int i = 0; i < 3; i++) {
+        while (countList[i]--) {
+            list[j++] = i + 1;
+        }
+    }
+    print(list, "countSort");
+}
+
 int main()
 {
     bubbleSort();
     selectionSort();
+    insertionSort();
+    countSort();
 }
